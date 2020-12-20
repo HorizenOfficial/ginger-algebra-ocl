f200886347959f6ee55f61a487a0306b3af637c5
Phoinic <max@phoinic.com.ua>
2020-12-20 22:12:29 +0200

    Cached programs initialization bugfix

--- algebra-kernels/src/polycommit.rs
+++ algebra-kernels/src/polycommit.rs
@@ -36,7 +36,7 @@ where
 
         unsafe {
             if !CACHED_PROGRAMS.contains_key(&d) {
-                CACHED_PROGRAMS.insert(d.clone(), HashMap::<HashMap<TypeId, opencl::Program>>::new());
+                CACHED_PROGRAMS.insert(d.clone(), HashMap::<TypeId, opencl::Program>::new());
             }
             if !CACHED_PROGRAMS.get(&d).unwrap().contains_key(&hash_key) {
                 CACHED_PROGRAMS.get_mut(&d).unwrap().insert(
